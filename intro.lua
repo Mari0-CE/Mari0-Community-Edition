ee2543bf1a4d183633e7313a88022ab948e40d2e
James Reynolds <magnusviri@me.com>
2020-08-21 02:00:37 -0600

    Made intro a table. Created a state table for update, draw, etc.

--- intro.lua
+++ intro.lua
@@ -17,13 +17,13 @@ function intro:update(dt)
 		if self.progress > self.duration+self.blackafterintro then
 			self.progress = self.duration+self.blackafterintro
 		end
-
+		
 		if self.progress > 0.5 and playedwilhelm == nil then
 			playsound("stab")
-
+			
 			playedwilhelm = true
 		end
-
+		
 		if self.progress == self.duration + self.blackafterintro then
 			self.finished()
 			shaders:set(1, shaderlist[currentshaderi1])
@@ -39,7 +39,7 @@ function intro:draw()
 	else
 		logoscale = 1
 	end
-
+	
 	if self.progress >= 0 and self.progress < self.duration then
 		local a = 1
 		if self.progress < self.fadetime then
@@ -47,22 +47,22 @@ function intro:draw()
 		elseif self.progress >= self.duration-self.fadetime then
 			a = (1-(self.progress-(self.duration-self.fadetime))/self.fadetime)
 		end
-
+		
 		love.graphics.setColor(1, 1, 1, a)
-
+		
 		if self.progress > self.fadetime+0.3 and self.progress < self.duration - self.fadetime then
 			local y = (self.progress-0.2-self.fadetime) / (self.duration-2*self.fadetime) * 206 * 5
 			love.graphics.draw(logo, love.graphics.getWidth()/2, love.graphics.getHeight()/2, 0, logoscale, logoscale, 142, 150)
 			love.graphics.setScissor(0, love.graphics.getHeight()/2+150*logoscale - y, love.graphics.getWidth(), y)
 			love.graphics.draw(logoblood, love.graphics.getWidth()/2, love.graphics.getHeight()/2, 0, logoscale, logoscale, 142, 150)
 			love.graphics.setScissor()
-
+			
 		elseif self.progress >= self.duration - self.fadetime then
 			love.graphics.draw(logoblood, love.graphics.getWidth()/2, love.graphics.getHeight()/2, 0, logoscale, logoscale, 142, 150)
 		else
 			love.graphics.draw(logo, love.graphics.getWidth()/2, love.graphics.getHeight()/2, 0, logoscale, logoscale, 142, 150)
 		end
-
+		
 		local a2 = math.max(0, (1-(self.progress-.5)/0.3))
 		love.graphics.setColor(0.6, 0.6, 0.6, a2)
 		properprint("loading mari0 ce..", love.graphics.getWidth()/2-string.len("loading mari0 ce..")*4*scale, love.graphics.getHeight()/2-170*logoscale-7*scale)
